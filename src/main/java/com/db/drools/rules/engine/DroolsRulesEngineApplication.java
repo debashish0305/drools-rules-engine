5cb245dca2fb68e8067bbc75694e41102059cfbe
debashishbarik <debashish.barik@hotmail.com>
2023-06-29 20:53:58 +0530

    Drools rules engine initial commits

---
+++ src/main/java/com/db/drools/rules/engine/DroolsRulesEngineApplication.java
@@ -0,0 +1,13 @@
+package com.db.drools.rules.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsRulesEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolsRulesEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/drools/rules/engine/DroolsRulesEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.drools.rules.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsRulesEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
